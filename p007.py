e244d042c9e147d205c275d80cadf52721153514
Joost van Dorp <joostvandorp@gmail.com>
2020-10-16 18:08:52 -0400

    solved problem 12

--- p007.py
+++ p007.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
+from eulerlibs.factors import factors
+
 
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
 # we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
-
-
-def factors(x):
-    # We will store all factors in `result`
-    result = []
-    i = 1
-    # This will loop from 1 to int(sqrt(x))
-    while i * i <= x:
-        # Check if i divides x without leaving a remainder
-        if x % i == 0:
-            result.append(i)
-            # Handle the case explained in the 4th
-            if x // i != i:  # In Python, // operator performs integer/floored division
-                result.append(x // i)
-        i += 1
-    # Return the list of factors of x
-    return result
-
 
 def is_prime(x):
     """
